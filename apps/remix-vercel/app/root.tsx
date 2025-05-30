c8bb7825956167fe62f124a415ad7027b981c46d
Santiago <sant.serrano@icloud.com>
2025-05-30 20:36:34 -0300

    fix: vercel deploy

--- apps/remix-vercel/app/root.tsx
+++ apps/remix-vercel/app/root.tsx
@@ -28,6 +28,8 @@ export const loader = async (args: LoaderFunctionArgs) => {
     ...authData,
     ENV: {
       API_URL: process.env.API_URL || "http://localhost:3001",
+      VAPID_PUBLIC_KEY: process.env.VAPID_PUBLIC_KEY,
+      INTERNAL_API_KEY: process.env.INTERNAL_API_KEY,
     },
   };
 };
