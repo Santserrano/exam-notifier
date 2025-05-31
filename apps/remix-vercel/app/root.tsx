9151da75235e960c5cd137b7624753445e887179
Santiago <sant.serrano@icloud.com>
2025-05-31 00:24:27 -0300

    fix: vercel deploy

--- apps/remix-vercel/app/root.tsx
+++ apps/remix-vercel/app/root.tsx
@@ -1,35 +1,28 @@
+// root.tsx
 import { ClerkApp } from "@clerk/remix";
 import { rootAuthLoader } from "@clerk/remix/ssr.server";
-import type { LoaderFunction, LinksFunction } from "@remix-run/node";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, LiveReload, useLoaderData } from "@remix-run/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  LiveReload,
+} from "@remix-run/react";
 
-import { customEs } from "./localizations/customEs";
 import fontStyles from "./styles/font.css?url";
 import tailwindStyles from "./styles/tailwind.css?url";
-import { getPublicEnv } from "./utils/env.server";
+import { customEs } from "./localizations/customEs";
+
+export const loader: LoaderFunction = rootAuthLoader;
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: tailwindStyles },
   { rel: "stylesheet", href: fontStyles },
 ];
 
-export const loader: LoaderFunction = async (args) => {
-  const authData = await rootAuthLoader(args);
-  const env = getPublicEnv();
-
-  if (!env.API_URL) {
-    throw new Error("API_URL no está definida en las variables de entorno");
-  }
-
-  return {
-    ...authData,
-    ENV: env,
-  };
-};
-
 function App() {
-  const { ENV } = useLoaderData<typeof loader>();
-
   return (
     <html lang="es" className="h-full">
       <head>
@@ -42,11 +35,6 @@ function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(ENV)};`,
-          }}
-        />
         <LiveReload />
       </body>
     </html>
@@ -54,6 +42,6 @@ function App() {
 }
 
 export default ClerkApp(App, {
-  localization: customEs,
   publishableKey: process.env.CLERK_PUBLISHABLE_KEY!,
+  localization: customEs,
 });
